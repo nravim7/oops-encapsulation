17e29cd9d909c2ba02b7c602c4398868292bd972
Ravi N <your.email@example.com>
2024-06-10 21:45:40 +0530

    Encapsulation part 1

---
+++ src/Player.java
@@ -0,0 +1,24 @@
+public class Player {
+    public String fullName;
+    public int health;
+    public String weapon;
+
+    public void loseHealth(int damage) {
+        health -= damage;
+        if (health <= 0) {
+            System.out.println("Player knocked out of game");
+        }
+    }
+
+    public void restoreHealth(int extraHealth) {
+        health += extraHealth;
+        if (health > 100) {
+            System.out.println("Player restored to 100%");
+            this.health = 100;
+        }
+    }
+
+    public int healthRemaining() {
+        return health;
+    }
+}
